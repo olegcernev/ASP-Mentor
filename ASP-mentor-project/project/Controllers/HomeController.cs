fb740db5fee570e7496f676313b01c5daf7b5731
Oleg Cernev <128173115+olegcernev@users.noreply.github.com>
2023-06-06 23:23:38 +0300

    asp-mentor

---
+++ ASP-mentor-project/project/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using project.Models;
+
+namespace project.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Courses()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult Events()
+    {
+        return View();
+    }
+    public IActionResult Pricing()
+    {
+        return View();
+    }
+    public IActionResult Trainers()
+    {
+        return View();
+    }
+
+    public IActionResult CoursesDetails()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
